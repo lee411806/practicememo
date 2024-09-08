7589c423967bdb3d229dae1f1474b71db24aff55
JDragon <lee411806@naver.com>
2024-09-08 19:29:12 +0900

    init commit

---
+++ src/main/java/com/sparta/memopractice2/Memopractice2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.memopractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memopractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Memopractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/memopractice2/Memopractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memopractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memopractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
